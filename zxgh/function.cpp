dfa45bd7bad35b85e8adbffb96d0c9dd1a75ce0a
zzttyy413841467 <zzttyy413841467@gmail.com>
2018-12-25 00:42:04 +0800

    初始段

--- zxgh/function.cpp
+++ zxgh/function.cpp
@@ -1,6 +1,6 @@
 #include "pch.h"
 
-vec f1(double e, vec x)
+vec dxde1(double e, vec x)
 {
 	double sigma = sig0_a * pi / 180 ;
 	double r = x(0);
@@ -15,3 +15,25 @@ vec f1(double e, vec x)
 	return xdot;
 }
 
+vec dxde2(double e, vec x)
+{
+	double sigma = sig0 / 180 * pi;
+
+	double r = x(0);
+	double theta = x(1);
+	double phi = x(2);
+	double gamma = x(3);
+	double psi = x(4);
+	double V = sqrt(2 * (1 / r - e));
+	vec aero = calcu_aero(r, V);
+	double L = aero(0);
+	double D = aero(1);
+	vec xdot(6);
+	xdot(0) = sin(gamma) / D;
+	xdot(1) = cos(gamma)*sin(psi) / (r*D*cos(phi));
+	xdot(2) = cos(gamma)*cos(psi) / (r*D);
+	xdot(3) = 1 / (D*V*V)*(L*cos(sigma) + (V*V / r - 1 / r / r)*cos(gamma));
+	xdot(4) = 1 / (D*V)*(L*sin(sigma) / V / cos(gamma) + V / r * cos(gamma)*sin(psi)*tan(phi));
+	xdot(5) = 1 / (D*V);
+	return xdot;
+}
--- zxgh/rk.cpp
+++ zxgh/rk.cpp
@@ -21,13 +21,48 @@ mat rk1(vec espan, vec x0)
 		{
 			break;
 		}
-		k1 = f1(e, x);
-		k2 = f1(e + h / 2, x + h * k1 / 2);
-		k3 = f1(e + h / 2, x + h * k2 / 2);
-		k4 = f1(e + h, x + h * k3);
+		k1 = dxde1(e, x);
+		k2 = dxde1(e + h / 2, x + h * k1 / 2);
+		k3 = dxde1(e + h / 2, x + h * k2 / 2);
+		k4 = dxde1(e + h, x + h * k3);
 		x = x + (h / 6)*(k1 + 2 * k2 + 2 * k3 + k4);
 	}
 	mat xx(n, 1 + x0.n_elem);
 	xx = join_rows(espan, x1);
 	return xx.rows(0, i-1);
+}
+
+mat rk2(double e0,double ef, vec x0)
+{
+	uword n = 2000;
+	vec espan = linspace(e0, ef, n);
+	double h = (ef - e0) / (n - 1);
+	double e = 0;
+
+	int result;
+	vec x = x0;
+	mat x1(n, x0.n_elem);
+	vec k1(6), k2(6), k3(6), k4(6);
+	uword i;
+	vec xxx(2);
+	for (i = 0; i < n; i++)
+	{
+		e = espan(i);
+		x1.row(i) = x.st();
+		xxx(0) = x(0);
+		xxx(1) = x(3);
+		result = trans(e, xxx);
+		if (result == 1)
+		{
+			break;
+		}
+		k1 = dxde2(e, x);
+		k2 = dxde2(e + h / 2, x + h * k1 / 2);
+		k3 = dxde2(e + h / 2, x + h * k2 / 2);
+		k4 = dxde2(e + h, x + h * k3);
+		x = x + (h / 6)*(k1 + 2 * k2 + 2 * k3 + k4);
+	}
+	mat xx(n, 1 + x0.n_elem);
+	xx = join_rows(espan, x1);
+	return xx.rows(0, i - 1);
 }
